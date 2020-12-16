ac2c51787984377a8b5e7e051eb586b5bc0eef69
WouterPaard <45482452+WouterPaard@users.noreply.github.com>
2020-12-16 12:05:50 +0100

    Add files via upload
    Programma 2 werkt toch blijkbaar niet want het geeft niet de hoogste waarde bij train_and_plot. Nu deze code die probeert elke mogelijke alpha en epsilon maar de waardes moeten handmatig worden afgelezen. Hoogste waarde blijkt te komen uit alpha 0,1 en epsilon 0,5

--- Machine learning python.py
+++ Machine learning python.py
@@ -15,16 +15,20 @@ class MyAgent(MLAgent):
     
     
 
-random.seed(1)
+for a in range(1, 5):
+    for e in range(1, 11):
+        random.seed(1)
+        print("a is ", (1/10))
+        print("e is ", (10/10))
+        my_agent = MyAgent(alpha=(1/10), epsilon=(10/10))
+        #my_agent = load('MyAgent_3000')
+        #my_agent.learning = False
 
-my_agent = MyAgent(alpha=(0.2), epsilon=0.8)
-
-random_agent = RandomAgent()
-train_and_plot(
-    agent=my_agent,
-    validation_agent=random_agent,
-    iterations=50,
-    trainings=100,
-    validations=1000)
-
+        random_agent = RandomAgent()
+        train_and_plot(
+            agent=my_agent,
+            validation_agent=random_agent,
+            iterations=100,
+            trainings=30,
+            validations=1000)
 
